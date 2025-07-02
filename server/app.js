7e1d64f22e219d56e6b2961d74063861d40150fa
Arseid <yvesrobert.estrada@gmail.com>
2025-07-02 10:52:15 +0200

    Add CORS middleware

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const v1UserRoutes = require("./v1/routes/userRoutes");
 const v1PostRoutes = require("./v1/routes/postRoutes");
 const { specs, swaggerUi } = require('./swagger');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/v1/users', v1UserRoutes);
 app.use('/v1/posts', v1PostRoutes);
